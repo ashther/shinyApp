cf91e921e8fe85e5a5b7ff6c1859107a71004a57
Sun Lijun <ashther@qq.com>
2016-04-14 16:45:20 +0800

    update

--- dashboard_test.R
+++ dashboard_test.R
@@ -3,7 +3,7 @@ library(shiny)
 library(shinydashboard)
 library(ggplot2)
 
-source('dataSource.R')
+# source('dataSource.R')
 
 ui <- dashboardPage(
   skin = 'black', 
@@ -13,29 +13,74 @@ ui <- dashboardPage(
   dashboardSidebar(
 
     sidebarMenu(
-      menuItem('Point', tabName = 'point', icon = icon('th')), 
-      menuItem('Login', tabName = 'login', icon = icon('dashboard')),
-      menuItem('Quick_chat', tabName = 'quick_chat', icon = icon('th'))
+      menuItem('Login', 
+               tabName = 'login', 
+               icon = icon('dashboard')),
+      menuItem('Quick_chat', 
+               tabName = 'quick_chat', 
+               icon = icon('th'))
     )
   ),
   
   dashboardBody(
       
     tabItems(
+
       tabItem(
-          tabName = 'point', 
+          tabName = 'login',
+          
+          # 第一行，实时用户数据
           fluidRow(
               valueBoxOutput('total_user'), 
               valueBoxOutput('new_user_today'), 
               valueBoxOutput('active_user_today')
-          )  
-      ), 
-
-      tabItem(
-          tabName = 'login',
-           
+          ), 
+          
+          # 第二行，历史用户数据
           fluidRow(
-            box(plotOutput('plot1'), width = 7, solidHeader = TRUE)  
+              
+              # 第二行第一列，历史数据图
+              column(
+                  width = 7, 
+                  box(plotOutput('plot_login'), 
+                      width = NULL, 
+                      solidHeader = TRUE)
+              ), 
+              
+              # 第二行第二列，绘图选项
+              column(
+                  width = 5, 
+                  
+                  # 小时/日/周/月选项
+                  box(selectInput('date_format', 
+                                  label = 'select time format', 
+                                  choices = list('hourly' = 'hourly', 
+                                                 'daily' = 'daily', 
+                                                 'weekly' = 'weekly', 
+                                                 'monthly' = 'monthly'), 
+                                  selected = 'hourly'), 
+                      width = NULL, 
+                      solidHeader = TRUE), 
+                  
+                  # 数据类型选项
+                  box(selectInput('data_type', 
+                                  label = 'select data type', 
+                                  choices = list('new' = 'new', 
+                                                 'active' = 'active', 
+                                                 'login' = 'login', 
+                                                 'retention' = 'retention'), 
+                                  selected = 'new'), 
+                      width = NULL, 
+                      solidHeader = TRUE), 
+                  
+                  # 时间范围选项
+                  box(dateRangeInput('date_range', 
+                                     label = 'select date range', 
+                                     min = min(daily_login$date_time), 
+                                     language = 'zh-CN'), 
+                      width = NULL, 
+                      solidHeader = TRUE)
+              )
           )
       ),
 
@@ -48,6 +93,17 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
+    
+    data_login <- reactive({
+        get(paste0(input$date_format, '_login'))
+    })
+    
+    data_login_range <- reactive({
+        subset(data_login(), 
+               as.Date(date_time) >= input$date_range[1] & 
+                   as.Date(date_time) <= input$date_range[2])
+    })
+    
     output$total_user <- renderValueBox({
         valueBox(point_data$value[point_data$item == 'total_user'], 'total user')
     })
@@ -60,9 +116,31 @@ server <- function(input, output) {
         valueBox(daily_login$active[daily_login$date_time == (Sys.Date() - 1)], 'active user')
     })
     
-    output$plot1 <- renderPlot({
-        ggplot(daily_login, aes(date_time, new)) + geom_bar(stat = 'identity')
+    output$plot_login <- renderPlot({
+        ggplot(data_login_range(), 
+               aes(date_time, 
+                   get(input$data_type))) + 
+            geom_bar(stat = 'identity')
     })
 }
 
 shinyApp(ui, server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- dataSource.R
+++ dataSource.R
@@ -35,6 +35,20 @@ while (dbMoreResults(con)) {
 }
 dbClearResult(res)
 
+res <- dbSendQuery(con, 'select * from weekly.login')
+weekly_login <- dbFetch(res, n = -1)
+while (dbMoreResults(con)) {
+    dbNextResult(con)
+}
+dbClearResult(res)
+
+res <- dbSendQuery(con, 'select * from monthly.login;')
+monthly_login <- dbFetch(res, n = -1)
+while (dbMoreResults(con)) {
+    dbNextResult(con)
+}
+dbClearResult(res)
+
 res <- dbSendQuery(con, 'select * from daily.quick_chat;')
 daily_quick_chat <- dbFetch(res, n = -1)
 while (dbMoreResults(con)) {
@@ -53,30 +67,26 @@ dbDisconnect(con)
 
 rm(con, res, host, port, username, password, dbname)
 
+
+# =============================================================================
+
 daily_login$date_time <- as.Date(daily_login$date_time)
-daily_login$time_stamp <- as.POSIXct(daily_login$time_stamp)
-
-daily_quick_chat$date_time <- as.Date(daily_quick_chat$date_time)
-daily_quick_chat$time_stamp <- as.POSIXct(daily_quick_chat$time_stamp)
-
 hourly_login$date_time <- strptime(hourly_login$date_time, 
                                    format = '%Y-%m-%d-%H') %>% as.POSIXct()
-hourly_login$time_stamp <- as.POSIXct(hourly_login$time_stamp)
-
-hourly_quick_chat$date_time <- strptime(hourly_quick_chat$date_time, 
-                                        format = '%Y-%m-%d-%H') %>% as.POSIXct()
-hourly_quick_chat$time_stamp <- as.POSIXct(hourly_quick_chat$time_stamp)
+weekly_login$first_weekday <- as.Date(weekly_login$first_weekday)
+monthly_login$first_monthday <- strptime(paste0(monthly_login$year_month_id, '-01'), 
+                                         format = '%Y-%m-%d') %>% as.Date()
+# =============================================================================
 
 dt <- data.frame(date_time = seq(from = min(daily_login$date_time),
-                                 to = Sys.Date() - 1, by = 1))
+                                 to = Sys.Date(), 
+                                 by = 'day'))
 
 daily_login <- left_join(dt, daily_login, by = 'date_time') %>%
     select(date_time, new, active, login, retention)
 daily_login[is.na(daily_login)] <- 0
 
-daily_quick_chat <- left_join(dt, daily_quick_chat, by = 'date_time') %>%
-    select(date_time, active_circle, message, active_user)
-daily_quick_chat[is.na(daily_quick_chat)] <- 0
+# =============================================================================
 
 temp_hr <- as.POSIXlt(Sys.time())
 temp_hr$hour <- temp_hr$hour - 1
@@ -89,14 +99,27 @@ hourly_login <- left_join(hr, hourly_login, by = 'date_time') %>%
     select(date_time, new, active, login)
 hourly_login[is.na(hourly_login)] <- 0
 
-hourly_quick_chat <- left_join(hr, hourly_quick_chat, by = 'date_time') %>% 
-    select(date_time, active_circle, message, active_user)
-hourly_quick_chat[is.na(hourly_quick_chat)] <- 0
+# =============================================================================
 
+wk <- data.frame(first_weekday = seq(from = min(weekly_login$first_weekday),
+                                     to = Sys.Date(),
+                                     by = 'week')) %>% 
+    mutate(year_week = paste0(format(Sys.time(), '%Y'), '-', format(first_weekday, '%V')))
+weekly_login <- left_join(wk, weekly_login, c('first_weekday', 'year_week')) %>% 
+    select(first_weekday, year_week, new, active, login)
+weekly_login[is.na(weekly_login)] <- 0
 
+# =============================================================================
 
+mt <- data.frame(first_monthday = seq(from = min(monthly_login$first_monthday), 
+                                      to = Sys.Date(), 
+                                      by = 'month')) %>% 
+    mutate(year_month_id = paste0(format(Sys.time(), '%Y'), '-', format(first_monthday, '%m')))
+monthly_login <- left_join(mt, monthly_login, c('first_monthday', 'year_month_id')) %>% 
+    select(first_monthday, year_month_id, new, active, login)
+monthly_login[is.na(monthly_login)] <- 0
 
-
+rm(dt, hr, wk, mt)
 
 
 

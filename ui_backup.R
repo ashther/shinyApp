69adbdde04dd1c8c318d87002e5a9af005adc335
Sun Lijun <ashther@qq.com>
2016-07-28 07:50:37 +0800

    update

--- ui_backup.R
+++ ui_backup.R
@@ -9,15 +9,15 @@ library(plotly)
 shinyUI(dashboardPage(
     skin = 'black', 
     
-    dashboardHeader(title = '数据分析'), 
+    dashboardHeader(
+    	title = '数据分析'
+    	), 
     
     dashboardSidebar(
-        
-    ),
+    	),
     
     dashboardBody(
-        
-        p('目前外网数据同步更新出现问题，请稍后', 
+    	p('目前外网数据同步更新出现问题，请稍后', 
           style = 'font-size:100%')
-    )
+    	)
 ))

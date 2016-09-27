fecee92e0dcead96dc33c54d4870be6955cf0309
Sun Lijun <ashther@qq.com>
2016-09-27 11:18:58 +0800

    plotly升级至4.5版本后接口有变化

--- whatIsOnServer/shinyApp/renderOutput_channel.R
+++ whatIsOnServer/shinyApp/renderOutput_channel.R
@@ -3,57 +3,45 @@ output$versionCode <- renderPlotly({
   temp <- count(channel_data(), version) %>% 
     arrange(n)
   plot_ly(temp, 
-          x = n, 
-          y = version, 
+          x = ~n, # plotly 4.5 
+          y = ~version, # plotly 4.5
           type = 'bar', 
           orientation = 'h') %>% 
     layout(title = '统计期内各版本用户数', 
            xaxis = list(title = '用户数'), 
            yaxis = list(title = '版本号', type = 'category'), 
-           magrin = list(l = 200))
+           margin = list(l = 200))
 })
 
 output$channelplot <- renderPlotly({
   temp <- count(channel_data(), channel) %>% 
     arrange(n)
   plot_ly(temp,
-          x = n,
-          y = channel,
+          x = ~n, # plotly 4.5
+          y = ~channel, # plotly 4.5
           type = 'bar', 
           orientation = 'h') %>%
     layout(title = '统计期内各渠道来源用户数',
            xaxis = list(title = '用户数'), 
            yaxis = list(title = '渠道'), 
-           marin = list(l = 200))
+           margin = list(l = 200))
 })
 
 output$channel_version_heatmap <- renderPlotly({
-  # temp <- count(channel_data(), channel, version)
-  # vals <- unique(scales::rescale(temp$n))
-  # o <- order(vals, decreasing = FALSE)
-  # cols <- scales::col_numeric("Blues", domain = NULL)(vals)
-  # colz <- setNames(data.frame(vals[o], cols[o]), NULL)
-  # plot_ly(as.data.frame(temp), 
-  #         x = channel, 
-  #         y = version,
-  #         z = n, 
-  #         type = 'heatmap', 
-  #         colorscale = colz) %>% 
-  #   layout(title = '统计期内各渠道及版本用户数情况', 
-  #          xaxis = list(title = '渠道', 
-  #                       size = 10), 
-  #          yaxis = list(title = '版本号'))
-  temp <- channel_data() %>% 
-    count(channel, version) %>% 
-    spread(key = version, value = n, fill = 0)
+  # plotly 4.5
+  temp <- as.data.frame(count(channel_data(), channel, version))
+  vals <- unique(scales::rescale(temp$n))
+  o <- order(vals, decreasing = FALSE)
+  cols <- scales::col_numeric("Blues", domain = NULL)(vals)
+  colz <- setNames(data.frame(vals[o], cols[o]), NULL)
   
-  plot_ly(x = temp$channel, 
-          y = colnames(temp)[-1], 
-          z = t(as.matrix(temp[, -1])), 
-          colorscale = 'pairs', 
-          colorbar = list(title = '用户数'), 
-          type = 'heatmap') %>% 
+  plot_ly(temp, 
+          x = ~channel, 
+          y = ~version, 
+          z = ~n, 
+          type = 'heatmap', 
+          colorscale = colz) %>% 
     layout(title = '统计期内各渠道及版本用户数情况', 
            xaxis = list(title = '渠道'), 
-           yaxis = list(title = '版本号', type = 'category'))
+           yaxis = list(title = '版本号'))
 })
--- whatIsOnServer/shinyApp/renderOutput_demographic.R
+++ whatIsOnServer/shinyApp/renderOutput_demographic.R
@@ -6,6 +6,7 @@ output$demographic_age_plot <- renderPlotly({
     plot_ly(x = demographic_age_data()$x, 
             y = demographic_age_data()$y, 
             mode = 'lines', 
+            type = 'scatter', # plotly 4.5
             fill = 'tozeroy') %>% 
       layout(title = sprintf('图1：%s年龄分布',
                              ifelse(input$demographic_university_select == '不限',
@@ -18,37 +19,53 @@ output$demographic_age_plot <- renderPlotly({
 
 output$demographic_gender_plot <- renderPlotly({
   plot_ly(demographic_gender_data(), 
-          labels = gender, 
-          values = n, 
+          labels = ~gender, # plotly 4.5
+          values = ~n, # plotly 4.5
           type = 'pie') %>% 
     layout(title = 
              sprintf('图2：%s性别占比',
                      ifelse(input$demographic_university_select == '不限',
                             '全体用户',
-                            input$demographic_university_select)))
+                            input$demographic_university_select)), 
+           xaxis = list(showline = FALSE, 
+                        zeroline = FALSE, 
+                        showgrid = FALSE, 
+                        showticklabels = FALSE), 
+           yaxis = list(showline = FALSE, 
+                        zeroline = FALSE, 
+                        showgrid = FALSE, 
+                        showticklabels = FALSE))
 })
 
 output$demographic_degree_plot <- renderPlotly({
   plot_ly(demographic_degree_data(), 
-          labels = degree, 
-          values = n, 
+          labels = ~degree, # plotly 4.5
+          values = ~n, # plotly 4.5
           type = 'pie') %>% 
     layout(title = 
              sprintf('图3：%s受教育程度占比',
                      ifelse(input$demographic_university_select == '不限',
                             '全体用户',
-                            input$demographic_university_select)))
+                            input$demographic_university_select)), 
+           xaxis = list(showline = FALSE, 
+                        zeroline = FALSE, 
+                        showgrid = FALSE, 
+                        showticklabels = FALSE), 
+           yaxis = list(showline = FALSE, 
+                        zeroline = FALSE, 
+                        showgrid = FALSE, 
+                        showticklabels = FALSE))
 })
 
 output$demographic_university_top10 <- renderPlotly({
   plot_ly(demographic_university_top10(), 
-          x = n, 
-          y = university, 
+          x = ~n, # plotly 4.5
+          y = ~university, # plotly 4.5
           type = 'bar', 
           orientation = 'h') %>% 
     layout(title = '图4：用户所属院校TOP10', 
            xaxis = list(title = '用户数'), 
-           yaxis = list(title = ''), 
+           yaxis = list(title = '', categoryarray = n), # plotly 4.5 
            margin = list(l = 150))
 })
 
